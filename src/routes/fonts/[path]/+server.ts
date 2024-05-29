8504acd7d546f109813fca443e7f82aa7051ccf6
Scott Tolinski <scott.tolinski@gmail.com>
2024-05-29 10:44:37 -0600

    adds better whitelist logic

---
+++ src/routes/fonts/[path]/+server.ts
@@ -0,0 +1,44 @@
+import type { RequestHandler } from './$types';
+export const GET: RequestHandler = async ({ request, platform, params }) => {
+	console.log(`[LOGGING FROM /hello]: Request came from ${request.url}`);
+
+	const origin = request.headers.get('Origin');
+	const { SCOTTS_FONTS } = platform?.env || {};
+
+	if (SCOTTS_FONTS) {
+		const allowed = await isAllowedDomain(origin!);
+
+		if (!allowed) {
+			return new Response('Forbidden', { status: 403 });
+		}
+
+		const font = await SCOTTS_FONTS.get(params.path, 'arrayBuffer');
+
+		if (!font) {
+			return new Response('Font not found', { status: 404 });
+		}
+
+		return new Response(font, {
+			headers: {
+				'Content-Type': 'font/woff2',
+				'Cache-Control': 'public, max-age=31536000',
+				'Access-Control-Allow-Origin': origin!,
+				'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+				'Access-Control-Allow-Headers': 'Content-Type'
+			}
+		});
+	}
+	return new Response('Not Available', { status: 401 });
+};
+
+async function isAllowedDomain(url: string) {
+	try {
+		const allowedDomains = JSON.parse((await SCOTTS_FONTS.get('domains')) || '[]') as string[];
+		const { hostname } = new URL(url);
+
+		return allowedDomains.some((domain) => hostname === domain || hostname.endsWith(`.${domain}`));
+	} catch (error) {
+		console.error('Invalid URL:', error);
+		return false;
+	}
+}
