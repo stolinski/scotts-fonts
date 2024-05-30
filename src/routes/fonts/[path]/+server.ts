14ef4e71077a91b9534f1f187ff0b86f7aeaeff9
Scott Tolinski <scott.tolinski@gmail.com>
2024-05-29 18:35:14 -0600

    adds more gratitous logs in an attempt to get this logging

--- src/routes/fonts/[path]/+server.ts
+++ src/routes/fonts/[path]/+server.ts
@@ -1,41 +1,47 @@
 import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async ({ request, platform, params }) => {
-	console.log(`[LOGGING FROM /]: Request came from ${request.url}`);
+	console.log('why arentyou running');
+	try {
+		console.log(`[LOGGING FROM /]: Request came from ${request.url}`);
 
-	const origin = request.headers.get('Referer');
-	const og = request.headers.get('Origin');
-	console.log('refer', origin);
-	console.log('og', og);
+		const origin = request.headers.get('Referer');
+		const og = request.headers.get('Origin');
+		console.log('refer', origin);
+		console.log('og', og);
 
-	const { SCOTTS_FONTS } = platform?.env || {};
+		const { SCOTTS_FONTS } = platform?.env || {};
 
-	if (SCOTTS_FONTS) {
-		const allowed = await is_allowed_domain(origin!, SCOTTS_FONTS);
+		if (SCOTTS_FONTS) {
+			const allowed = await is_allowed_domain(origin!, SCOTTS_FONTS);
 
-		if (!allowed) {
-			return new Response('Forbidden', { status: 403 });
-		}
-
-		const font = await SCOTTS_FONTS.get(params.path, 'arrayBuffer');
-
-		if (!font) {
-			return new Response('Font not found', { status: 404 });
-		}
-
-		// This is here because Safari doesn't reliably set the Origin header, referer has "/" at the end causing cors issues.
-		const allow_origin = origin?.endsWith('/') ? origin.slice(0, -1) : origin;
-		console.log('allow_origin', allow_origin);
-		return new Response(font, {
-			headers: {
-				'Content-Type': 'font/woff2',
-				'Cache-Control': 'public, max-age=31536000',
-				'Access-Control-Allow-Origin': allow_origin!,
-				'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
-				'Access-Control-Allow-Headers': 'Content-Type'
+			if (!allowed) {
+				return new Response('Forbidden', { status: 403 });
 			}
-		});
+
+			const font = await SCOTTS_FONTS.get(params.path, 'arrayBuffer');
+
+			if (!font) {
+				return new Response('Font not found', { status: 404 });
+			}
+
+			// This is here because Safari doesn't reliably set the Origin header, referer has "/" at the end causing cors issues.
+			const allow_origin = origin?.endsWith('/') ? origin.slice(0, -1) : origin;
+			console.log('allow_origin', allow_origin);
+			return new Response(font, {
+				headers: {
+					'Content-Type': 'font/woff2',
+					'Cache-Control': 'public, max-age=31536000',
+					'Access-Control-Allow-Origin': allow_origin!,
+					'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+					'Access-Control-Allow-Headers': 'Content-Type'
+				}
+			});
+		}
+		return new Response('Not Available', { status: 401 });
+	} catch (error) {
+		console.error(error);
+		return new Response('Internal Server Error', { status: 500 });
 	}
-	return new Response('Not Available', { status: 401 });
 };
 
 async function is_allowed_domain(url, SCOTTS_FONTS) {
